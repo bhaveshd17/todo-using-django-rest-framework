d992192c010868f4f8d4d1c841d7b14afa281c13
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-06-22 16:07:08 +0530

    sigin login done

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,10 +3,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-	path('', views.toDoList, name="toDoList"),
+	path('index/', views.toDoList, name="toDoList"),
 	path('create/', views.createList, name="create"),
 	path('edit/<str:pk>', views.updateList, name="edit"),
 	path('delete/<str:pk>', views.deleteList, name="delete"),
-	path('loginUser/', views.loginUser, name='loginUser'),
-	path('signinUser/', views.signInUser, name='signInUser')
+	path('', views.loginUser, name='loginUser'),
+	path('signinUser/', views.signInUser, name='signInUser'),
+	path('logout/', views.logoutUser, name='logout'),
+	path('view/', views.view_api, name='view')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,22 +1,33 @@
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 import requests
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user
 
+from .forms import UserForm
 
 # domain = 'https://todobhavesh.herokuapp.com'
 domain = 'http://127.0.0.1:8000'
 
+@login_required(login_url='loginUser')
 def toDoList(request):
     response = requests.get(domain+'/api/task-list/')
     data = response.json()
     content = {'data':data}
     return render(request, 'frontend/index.html', content)
 
+
+@login_required(login_url='loginUser')
 def createList(request):
     urlPost = domain+'/api/task-create/'
     title = request.POST.get('task')
     requests.post(urlPost, {'title': title})
-    return redirect('/')
+    return redirect('/index/')
 
+
+@login_required(login_url='loginUser')
 def updateList(request, pk):
     urlUpdate = domain+f'/api/task-update/{pk}/'
     response = requests.get(domain + '/api/task-list/')
@@ -27,28 +38,54 @@ def updateList(request, pk):
     if request.method == 'POST':
         task = request.POST.get('task')
         requests.put(urlUpdate, {'title': task})
-        return redirect('/')
+        return redirect('/index/')
     return render(request, 'frontend/edit.html', content)
 
+
+@login_required(login_url='loginUser')
 def deleteList(request, pk):
     deleteUrl = domain+f'/api/task-delete/{pk}/'
     requests.delete(deleteUrl)
-    return redirect('/')
+    return redirect('/index/')
 
+
+@login_required(login_url='loginUser')
+def view_api(request):
+    return redirect('/api/task-list/')
 
 #authentication
-
+@unauthenticated_user
 def signInUser(request):
-    content = {
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+            login(request, user)
+            request.session.set_expiry(60 * 60 * 24 * 30)
+            return redirect('/index/')
+    else:
+        form = UserForm()
 
-    }
-    return render(request, 'auth/signin.html', content)
+    return render(request, 'auth/signin.html', {'form':form})
 
+@unauthenticated_user
 def loginUser(request):
-    content = {
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            request.session.set_expiry(60 * 60 * 24*30)
+            return redirect('/index/')
+        else:
+            messages.error(request, 'Wrong username or password')
+            return render(request, 'auth/login.html')
+    return render(request, 'auth/login.html')
 
-    }
-    return render(request, 'auth/login.html', content)
 
-def logout(request):
+def logoutUser(request):
+    logout(request)
     return redirect('/')
